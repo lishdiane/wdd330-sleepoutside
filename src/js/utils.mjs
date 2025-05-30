c5d887afd961fb8547b4d35d1942c96b8879b9eb
Vitalina Syzova <sizovavitalina@gmail.com>
2025-05-30 10:09:14 -0600

    individual activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -105,4 +105,26 @@ function updateCartCount() {
   } else {
     badge.style.display = "inline-block";
   }
+}  
+
+export function alertMessage(message, scroll = true, duration = 3000) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  const main = document.querySelector("main");
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName === "SPAN") {
+      main.removeChild(this);
+    }
+  });
+
+  main.prepend(alert);
+  if (scroll) window.scrollTo(0, 0);
+}
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
 }
