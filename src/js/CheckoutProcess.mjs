37d03f1f9e76e65e7d898e26029f9363aeb38f1d
lishdiane <dlish@byupathway.edu>
2025-05-29 21:24:23 -0600

    changes

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,31 +1,54 @@
+import { getLocalStorage } from "./utils.mjs";
+
 export default class CheckoutProcess {
     constructor() {
+        this.total = 0;
+        this.tax = 0;
+        this.shipping = 0;
+        this.orderTotal = 0;
+    }
+
+    init() {
+        this.countSubtotal();
+        this.countTax();
+        this.countShippingEstimate();
+        this.calculateOrderTotal();
+        this.displaySummary();
     }
 
     countSubtotal() {
         const cartItems = getLocalStorage("so-cart") || [];
-        let total = 0;
         for (const item of cartItems) {
         const quantity = item.quantity;
-        total += (Number(item.FinalPrice) || 0) * quantity;
+        this.total += (Number(item.FinalPrice) || 0) * quantity;
         }
-        return total;
     }
 
     countTax() {
-        let subTotal = this.countSubtotal();
-        let taxTotal = subTotal * 1.06;
-        return taxTotal;
+        this.tax = this.total * 0.06;  
     }
 
     countShippingEstimate() {
         const cartItems = getLocalStorage("so-cart") || [];
-        let total = [];
+        let total = 0;
         for (const item of cartItems) {
             total += item.quantity;
         }
         
-        let len = total.length;
+        if (cartItems.length > 0) {
+            this.shipping = 10 + ((total - 1) * 2);
+        }
         
+    }
+
+    calculateOrderTotal() {
+        this.Ordertotal = this.total + this.tax.toFixed(2) + this.shipping;
+    }
+
+    displaySummary() {
+        document.querySelector(".subtotal").innerHTML = `Subtotal: $${this.total}`;
+        document.querySelector(".tax").innerHTML = `Tax: $${this.tax.toFixed(2)}`;
+        document.querySelector(".shipestimate").innerHTML = `Shipping: $${this.shipping}`;
+        document.querySelector(".total").innerHTML = `Total: $${this.total}`;
     }
 }
