87d2e9f51f079c56249be24e5b0cbca321d3c65a
lishdiane <dlish@byupathway.edu>
2025-05-31 16:34:11 -0600

    individual assignment initial commit

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -106,3 +106,28 @@ function updateCartCount() {
     badge.style.display = "inline-block";
   }
 }
+
+//Alert Message Pop-up
+export function alertMessage(message, scroll = true) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      main.removeChild(this);
+    }
+  });
+
+  const main = document.querySelector("main");
+  main.prepend(alert)
+
+  if (scroll)
+    window.scrollTo(0, 0);
+}
+
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
+}
